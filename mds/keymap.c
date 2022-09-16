e04c1673f038807655efb6e802bdd104b37f136b
Marco Dalla Stella <marco@7bridges.eu>
2022-09-16 15:17:28 +0200

    Tapping \ twice is C-c (Tap-dance)

--- mds/keymap.c
+++ mds/keymap.c
@@ -7,8 +7,8 @@ enum {
 
 // Tap Dance definition
 qk_tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for \, twice for C-z
-  [TD_BSLS_EMACS] = ACTION_TAP_DANCE_DOUBLE(KC_BSLS, LGUI(KC_S))
+    // Tap once for \, twice for C-c
+  [TD_BSLS_EMACS] = ACTION_TAP_DANCE_DOUBLE(KC_BSLS, LCTL(KC_C))
 };
 
 // Layers
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                    KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN,
                    KC_Z, KC_X, KC_C, KC_V, KC_B, KC_GRV, TD(TD_BSLS_EMACS), KC_N,
                    KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ESC, KC_LCTL, KC_LGUI,
-                   LALT_T(KC_RALT), KC_LSFT, KC_SPC, KC_BSPC, MO(_L1), MO(_L2),
+                   MO(_L2), KC_LSFT, KC_SPC, KC_BSPC, MO(_L1), LALT_T(KC_RALT),
                    KC_MINS, KC_QUOT, KC_ENT),
 
     [_L1] = LAYOUT(KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
