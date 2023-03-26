75a1d2974df885a6879c0a800a9774d76a85b67f
Marco Dalla Stella <marco@7bridges.eu>
2023-03-26 11:24:14 +0200

    [rev41] Small changes

--- mds/reviung41/keymap.c
+++ mds/reviung41/keymap.c
@@ -31,10 +31,10 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_reviung41
-  (KC_ESC,   KC_Q,   KC_W,    KC_E,   KC_R,     KC_T,            KC_Y,   KC_U,   KC_I,     KC_O,    KC_P,     KC_BSPC,
-   KC_LSFT,  KC_A,   KC_S,    KC_D,   KC_F,     KC_G,            KC_H,   KC_J,   KC_K,     KC_L,    KC_SCLN,  RSFT_T(KC_QUOT),
-   KC_LGUI,  KC_Z,   KC_X,    KC_C,   KC_V,     KC_B,            KC_N,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH,  KC_ENT,
-                                      KC_LCTL,  RAISE,  KC_SPC,  LOWER,  LRALT),
+  (KC_ESC,   KC_Q,          KC_W,           KC_E,     KC_R,     KC_T,             KC_Y,   KC_U,   KC_I,     KC_O,           KC_P,            KC_BSPC,
+   KC_LSFT,  CTL_T(KC_A),   ALT_T(KC_S),    KC_D,     KC_F,     KC_G,             KC_H,   KC_J,   KC_K,     ALT_T(KC_L),    CTL_T(KC_SCLN),  SFT_T(KC_SLSH),
+   KC_LGUI,  KC_Z,          KC_X,           KC_C,     KC_V,     KC_B,             KC_N,   KC_M,   KC_COMM,  KC_DOT,         KC_QUOT,         KC_ENT,
+                                                      KC_LCTL,  RAISE,   KC_SPC,  LOWER,  LRALT),
 
   [_LOWER] = LAYOUT_reviung41
   (KC_TRNS,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,               KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL,
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT_reviung41
   (KC_TRNS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,            KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_DEL,
-   KC_TRNS,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,
+   KC_CAPS,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,
    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_GRV,   KC_QUOT,            KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
                                            KC_TRNS,  KC_TRNS,  KC_BSPC,  KC_TRNS,  KC_TRNS),
 
