9972a97ca73db0cf0da7bece53521308aaa1fc93
Marco Dalla Stella <marco@7bridges.eu>
2023-04-14 09:29:16 +0200

    [3w6] Try to switch CTRL and SHIFT

--- mds/3w6/keymap.c
+++ mds/3w6/keymap.c
@@ -11,33 +11,40 @@ enum layers {
 
 #define MV_LEFT   SGUI(KC_LEFT)
 #define MV_RIGHT  SGUI(KC_RGHT)
+#define Z_CTL     LCTL_T(KC_Z)
+#define SLSH_CTL  RCTL_T(KC_SLSH)
+#define ESC_SFT   LSFT_T(KC_ESC)
+#define SPC_SYM   LT(_SYM, KC_SPC)
+#define TAB_NUM   LT(_NUM, KC_TAB)
+#define BSPC_NAV  LT(_NAV, KC_BSPC)
+#define ALT       LALT_T(KC_RALT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format off
 
     [_MAIN] = LAYOUT_split_3x5_3(
-        KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,         KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        LCTL_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,                              KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
-                      LSFT_T(KC_ESC),   LT(_SYM, KC_SPC), LT(_NUM, KC_TAB),  KC_ENT, LT(_NAV, KC_BSPC), LALT_T(KC_RALT)
+        KC_Q,   KC_W,  KC_E,  KC_R,     KC_T,                        KC_Y,      KC_U,  KC_I,     KC_O,    KC_P,
+        KC_A,   KC_S,  KC_D,  KC_F,     KC_G,                        KC_H,      KC_J,  KC_K,     KC_L,    KC_SCLN,
+        Z_CTL,  KC_X,  KC_C,  KC_V,     KC_B,                        KC_N,      KC_M,  KC_COMM,  KC_DOT,  SLSH_CTL,
+                              ESC_SFT,  SPC_SYM,  TAB_NUM,  KC_ENT,  BSPC_NAV,  ALT
     ),
     [_SYM] = LAYOUT_split_3x5_3(
-        KC_EXLM , KC_AT,   KC_HASH,  KC_DLR, KC_PERC,   KC_CIRC, KC_AMPR, KC_ASTR, KC_MINUS, KC_UNDS,
-        KC_SLSH, KC_LBRC, KC_LCBR, KC_LPRN,  KC_EQL,    KC_PIPE, KC_RPRN, KC_RCBR, KC_RBRC, KC_BSLS,
-        KC_TRNS, KC_PLUS, KC_GRV, KC_QUOTE, S(KC_QUOT), KC_NO, KC_NO, KC_MINS, KC_QUES, KC_TRNS,
-                          KC_NO, KC_NO, KC_NO,          KC_NO, KC_NO, KC_NO
+        KC_EXLM,   KC_AT,     KC_HASH,   KC_DLR,     KC_PERC,                          KC_CIRC,   KC_AMPR,   KC_ASTR,   KC_MINUS,  KC_UNDS,
+        KC_SLSH,   KC_LBRC,   KC_LCBR,   KC_LPRN,    KC_TILD,                          KC_PIPE,   KC_RPRN,   KC_RCBR,   KC_RBRC,   KC_BSLS,
+        KC_TRNS,   KC_PLUS,   KC_GRV,    KC_QUOTE,   S(KC_QUOT),                       KC_EQL,    KC_PLUS,   KC_MINS,   KC_SLSH,   KC_TRNS,
+                                         KC_TRNS,    KC_NO,        KC_NO,   KC_NO,     KC_NO,     KC_TRNS
     ),
     [_NAV] = LAYOUT_split_3x5_3(
-        KC_NO, KC_VOLD, KC_MUTE, KC_VOLU, KC_NO,      KC_HOME, KC_END,  KC_PGUP, KC_PGDN,  KC_DEL,
-        KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT, KC_NO,    KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,  KC_END,
-        KC_NO, KC_NO, KC_NO, KC_NO, MV_LEFT,          MV_RIGHT, KC_NO, KC_NO, KC_NO, KC_NO,
-                          KC_NO, KC_LGUI, KC_NO,      KC_NO, KC_NO, KC_NO
+        KC_NO,     KC_VOLD,   KC_MUTE,   KC_VOLU,   KC_NO,                       KC_HOME,    KC_END,    KC_PGUP,   KC_PGDN,   KC_DEL,
+        KC_MPRV,   KC_MPLY,   KC_MSTP,   KC_MNXT,   KC_NO,                       KC_LEFT,    KC_DOWN,   KC_UP,     KC_RGHT,   KC_END,
+        KC_NO,     KC_NO,     KC_NO,     KC_NO,     MV_LEFT,                     MV_RIGHT,   KC_NO,     KC_NO,     KC_NO,     KC_NO,
+                                         KC_NO,     KC_LGUI,   KC_NO,   KC_NO,   KC_NO,      KC_NO
     ),
     [_NUM] = LAYOUT_split_3x5_3(
-        KC_NO,  KC_F9, KC_F10, KC_F11, KC_F12,        KC_7,  KC_8,  KC_9, KC_PLUS, KC_ASTR,
-        KC_NO,  KC_F5,  KC_F6,  KC_F7,  KC_F8,        KC_4,  KC_5,  KC_6, KC_MINS, KC_SLSH,
-        KC_NO,  KC_F1,  KC_F2,  KC_F3,  KC_F4,        KC_1,  KC_2,  KC_3, KC_EQL, KC_PERC,
-                           KC_NO, KC_NO, KC_NO,       KC_DOT, KC_0, KC_NO
+        KC_NO,   KC_F9,   KC_F10,   KC_F11,   KC_F12,                     KC_7,   KC_8,   KC_9,   KC_PLUS,   KC_ASTR,
+        KC_NO,   KC_F5,   KC_F6,    KC_F7,    KC_F8,                      KC_4,   KC_5,   KC_6,   KC_MINS,   KC_SLSH,
+        KC_NO,   KC_F1,   KC_F2,    KC_F3,    KC_F4,                      KC_1,   KC_2,   KC_3,   KC_EQL,    KC_PERC,
+                                    KC_NO,    KC_NO,   KC_NO,   KC_DOT,   KC_0,   KC_NO
     ),
     // clang-format on
 };
