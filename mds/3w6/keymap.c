72f2dfa627b06ae84d7846da661d91a37af8dd66
Marco Dalla Stella <marco@7bridges.eu>
2023-04-15 17:44:06 +0200

    [3w6] Introduce IGNORE_MOD_TAP_INTERRUPT

--- mds/3w6/keymap.c
+++ mds/3w6/keymap.c
@@ -11,11 +11,12 @@ enum layers {
 
 #define MV_LEFT   SGUI(KC_LEFT)
 #define MV_RIGHT  SGUI(KC_RGHT)
-#define Z_CTL     LCTL_T(KC_Z)
-#define SLSH_CTL  RCTL_T(KC_SLSH)
-#define ESC_SFT   LSFT_T(KC_ESC)
+#define A_MOD     CTL_T(KC_A)
+#define SCLN_MOD  CTL_T(KC_SCLN)
+#define Z_MOD     SFT_T(KC_Z)
+#define SLSH_MOD  RSFT_T(KC_SLSH)
+#define ESC_NUM   LT(_NUM, KC_ESC)
 #define SPC_SYM   LT(_SYM, KC_SPC)
-#define TAB_NUM   LT(_NUM, KC_TAB)
 #define BSPC_NAV  LT(_NAV, KC_BSPC)
 #define ALT       LALT_T(KC_RALT)
 
@@ -24,9 +25,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_MAIN] = LAYOUT_split_3x5_3(
         KC_Q,   KC_W,  KC_E,  KC_R,     KC_T,                        KC_Y,      KC_U,  KC_I,     KC_O,    KC_P,
-        KC_A,   KC_S,  KC_D,  KC_F,     KC_G,                        KC_H,      KC_J,  KC_K,     KC_L,    KC_SCLN,
-        Z_CTL,  KC_X,  KC_C,  KC_V,     KC_B,                        KC_N,      KC_M,  KC_COMM,  KC_DOT,  SLSH_CTL,
-                              ESC_SFT,  SPC_SYM,  TAB_NUM,  KC_ENT,  BSPC_NAV,  ALT
+        A_MOD,  KC_S,  KC_D,  KC_F,     KC_G,                        KC_H,      KC_J,  KC_K,     KC_L,    SCLN_MOD,
+        Z_MOD,  KC_X,  KC_C,  KC_V,     KC_B,                        KC_N,      KC_M,  KC_COMM,  KC_DOT,  SLSH_MOD,
+                              ESC_NUM,  SPC_SYM,  KC_TAB,  KC_ENT,   BSPC_NAV,  ALT
     ),
     [_SYM] = LAYOUT_split_3x5_3(
         KC_EXLM,   KC_AT,     KC_HASH,   KC_DLR,     KC_PERC,                          KC_CIRC,   KC_AMPR,   KC_ASTR,   KC_MINUS,  KC_UNDS,
