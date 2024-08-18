f0ce677132a2deee00f566c34b9eac04e992e005
Marco Dalla Stella <marco@7bridges.eu>
2024-08-18 21:33:19 +0200

    Revert "[sweep] Switch SHIFT with CTRL"
    This reverts commit 722fc7061434ef17c3856329a9eaa45487f48222.

--- mds/sweep/keymap.c
+++ mds/sweep/keymap.c
@@ -1,8 +1,8 @@
 #include QMK_KEYBOARD_H
 
-#define CTL_A    LCTL_T(KC_A)
-#define CTL_SCLN LCTL_T(KC_SCLN)
-#define TAB_SFT  LSFT_T(KC_TAB)
+#define SFT_A    LSFT_T(KC_A)
+#define SFT_SCLN LSFT_T(KC_SCLN)
+#define TAB_CTL  LCTL_T(KC_TAB)
 #define ENT_ALT  LALT_T(KC_ENT)
 #define BSCP_LT1 LT(1,KC_BSPC)
 #define SPC_LT2  LT(2,KC_SPC)
@@ -13,9 +13,9 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x5_2(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        CTL_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    CTL_SCLN,
+        SFT_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    SFT_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                   TAB_SFT, SPC_LT2, BSCP_LT1, ENT_ALT
+                                   TAB_CTL, SPC_LT2, BSCP_LT1, ENT_ALT
     ),
     [1] = LAYOUT_split_3x5_2(
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
